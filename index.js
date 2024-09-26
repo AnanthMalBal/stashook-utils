000c682fb23558b3eb1b25bf64b93a935e7aba9a
AnanthMalBal <anandb.hbs@gmail.com>
2024-09-26 15:58:28 +0530

    Utility Files Initial Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const Util = require('./src/config/util');
+const Helper = require('./src/config/helper');
+const Model = require('./src/model/model');
+const Connection = require('./src/config/db-connect');
+
+module.exports = 
+{
+    Util, Helper, Model, Connection
+}
