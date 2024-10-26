57f7c39fc5c1df9de7f65e72f01528456a6a31ed
AnanthMalBal <anandb.hbs@gmail.com>
2024-10-26 22:11:15 +0530

    Date Range Implementation

--- src/config/util.js
+++ src/config/util.js
@@ -14,6 +14,23 @@ module.exports = {
         return moment(Date.now()).format(_DateFormat);
     },
 
+    getDateRange(fromDate, toDate) {
+        if (moment(fromDate, 'YYYY-MM-DD').isSame(moment(toDate, 'YYYY-MM-DD'), 'day'))
+            return [toDate];
+        let tmpDate = fromDate;
+        const dates = [tmpDate];
+        do {
+            tmpDate = moment(tmpDate).add(1, 'day');
+            dates.push(tmpDate.format('YYYY-MM-DD'));
+        } while (moment(tmpDate).isBefore(toDate));
+        return dates;
+    },
+
+    getDateRangeByDays(fromDate, noOfDays) {
+        let toDate = moment(fromDate).add(noOfDays, 'day');
+        return getDateRange(fromDate, toDate);
+    },
+
     withPercent(data) {
         return "%" + data + "%";
     },
